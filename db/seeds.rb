0c832df5f1aee5ad4d5646905be2c074a0a3aa6a
jandersen770 <jandersen770@gmail.com>
2021-04-22 22:53:12 -0400

    update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,62 +6,62 @@ Book.create!([
         image: "/Alice_in_Wonderland_cover.jpg",
         title: "Alice in Wonderland",
         location: "0",
-        user_id: 3
+        user_id: 1
     },
     {
         epub_file: "/leo-tolstoy_anna-karenina_constance-garnett.epub",
         image: "/ltak-cover.jpg",
         title: "Anna Karenina",
         location: "0",
-        user_id: 3
+        user_id: 1
     },
     {
         epub_file: "/mary-shelley_frankenstein.epub",
         image: "/frankenstein_image.jpg",
         title: "Frankenstein",
         location: "0",
-        user_id: 3
+        user_id: 1
     },
     {
         epub_file: "/charles-dickens_a-tale-of-two-cities.epub",
         image: "/2_Cities_image.jpg",
         title: "A Tale of Two Cities",
         location: "0",
-        user_id: 3
+        user_id: 1
     },
     {
         epub_file: "/walden.epub",
         image: "/walden.jpeg",
         title: "Walden",
         location: "0",
-        user_id: 3
+        user_id: 1
     },
     {
         epub_file: "/ludwig-wittgenstein_tractatus-logico-philosophicus.epub",
         image: "/ludwig-wittgenstein_tractatus-logico-philosophicus.jpeg",
         title: "Tractatus Logico Philosophicus",
         location: "0",
-        user_id: 3
+        user_id: 1
     },
     {
         epub_file: "/beowulf.epub",
         image: "/beowulf.jpeg",
         title: "Beowulf",
         location: "0",
-        user_id: 3
+        user_id: 1
     },
     {
         epub_file: "/yevgeny-zamyatin_we_gregory-zilboorg.epub",
         image: "/yevgeny-zamyatin_we_gregory-zilboorg.jpeg",
         title: "We",
         location: "0",
-        user_id: 3
+        user_id: 1
     },
     {
         epub_file: "/john-muir_my-first-summer-in-the-sierra.epub",
         image: "/john-muir_my-first-summer-in-the-sierra.jpeg",
         title: "My First Summer in the Sierra",
         location: "0",
-        user_id: 3
+        user_id: 1
     },
 ])
