75e4b605359f3644f7c3f8755adca23f89ebc9b1
jandersen770 <jandersen770@gmail.com>
2021-04-26 10:11:08 -0400

    Delete notifications and likes associated with comments that are deleted

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -46,6 +46,7 @@ class Api::CommentsController < ApplicationController
   # DELETE /comments/1.json
   def destroy
     @comment.notifications.destroy_all
+    recursive_delete(@comment.comments)
     @comment.destroy
     respond_to do |format|
       format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
