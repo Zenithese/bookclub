d0d52f3ba0c9eace9a527a2b8d5df615098377e1
Justin <jandersen770@gmail.com>
2023-06-20 16:41:54 -0700

    Update bootsnap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
